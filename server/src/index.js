ec8b3286c7a0ee410dfed70ed888318b15e0188c
cs2812 <cs786786786@gmail.com>
2023-07-20 13:16:34 +0530

    backend part done

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const dotenv = require("dotenv")
 const userRoute = require("../routes/userRoutes");
 const employeeRoute = require("../routes/authRoutes");
 require('dotenv').config();
